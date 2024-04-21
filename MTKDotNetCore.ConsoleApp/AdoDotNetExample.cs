2a6057c88288bc1c76c2c9c3de2b6a69b1de6fe8
minthukha <minthukha1239788@gmail.com>
2024-04-21 22:34:25 +0630

    Complete - AdoDotNet CRUD

---
+++ MTKDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCore.ConsoleApp
+{
+    public class AdoDotNetExample
+    {
+        #region Connection
+
+        public readonly SqlConnectionStringBuilder _stringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "MTKDotNetCore",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+
+        #endregion
+
+        #region Read
+
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_stringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = "select * from Tbl_Blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("BlogId => " + dr["BlogId"]);
+                Console.WriteLine("BlogTitle => " + dr["BlogTitle"]);
+                Console.WriteLine("BlogAuthor => " + dr["BlogAuthor"]);
+                Console.WriteLine("BlogContent => " + dr["BlogContent"]);
+                Console.WriteLine("---------------------------------------");
+            }
+        }
+
+#endregion
+
+        #region Edit
+
+        public void Edit(int id)
+        {
+            SqlConnection connection = new SqlConnection(_stringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = "select * from Tbl_Blog where BlogId=@BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                Console.WriteLine("No Data Found");
+                return;
+            }
+
+            DataRow dr = dt.Rows[0];
+            Console.WriteLine("BlogId => " + dr["BlogId"]);
+            Console.WriteLine("BlogTitle => " + dr["BlogTitle"]);
+            Console.WriteLine("BlogAuthor => " + dr["BlogAuthor"]);
+            Console.WriteLine("BlogContent => " + dr["BlogContent"]);
+            Console.WriteLine("---------------------------------------");
+        }
+#endregion
+
+        #region Create
+
+        public void Create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(_stringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+                            ([BlogTitle],
+                            [BlogAuthor],
+                            [BlogContent])
+                            VALUES
+                (@BlogTitle,
+                @BlogAuthor,
+                @BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            string message = result > 0 ? "New Blog Creation Successful" : "New Blog Creation Fail";
+            Console.WriteLine(message);
+        }
+
+#endregion
+
+        #region Update
+
+        public void Update(int id, string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(_stringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+                            SET [BlogTitle] = @BlogTitle
+                            ,[BlogAuthor] = @BlogAuthor
+                            ,[BlogContent] = @BlogContent
+                            WHERE BlogId=@BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            string message = result > 0 ? "Blog update successful" : "Blog update fail";
+            Console.WriteLine(message);
+        }
+
+#endregion
+
+        #region Delete
+
+        public void Delete(int id)
+        {
+            SqlConnection connection = new SqlConnection(_stringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"DELETE FROM [dbo].[Tbl_Blog]
+                          WHERE BlogId=@BlogId";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            string message = result > 0 ? "Blog delete successful" : "Blog delete fail";
+            Console.WriteLine(message);
+        }
+
+        #endregion
+
+    }
+}
--- MTKDotNetCore.ConsoleApp/Program.cs
+++ MTKDotNetCore.ConsoleApp/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using MTKDotNetCore.ConsoleApp;
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Update(1,"Hello","Hello","Hello");
+//adoDotNetExample.Edit(1);
+//adoDotNetExample.Delete(1);
+//adoDotNetExample.Edit(1);
+adoDotNetExample.Create("Lemon", "Lemon", "Lemon");
+adoDotNetExample.Read();
