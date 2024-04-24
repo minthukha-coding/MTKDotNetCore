a060265a562d25ef7d36bcd4d8cea5e542a50b1e
minthukha <minthukha1239788@gmail.com>
2024-04-25 00:23:54 +0630

    [ Testing ] - Creating a Model for an Existing Database in Entity Framework Core

---
+++ MTKDotNetCore.ConsoleApp/Models/MtkdotNetCoreContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MTKDotNetCore.ConsoleApp.Models;
+
+public partial class MtkdotNetCoreContext : DbContext
+{
+    public MtkdotNetCoreContext()
+    {
+    }
+
+    public MtkdotNetCoreContext(DbContextOptions<MtkdotNetCoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblBlog> TblBlogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=MTKDotNetCore;User ID = sa;Password = sasa@123;TrustServerCertificate=True;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblBlog>(entity =>
+        {
+            entity.HasKey(e => e.BlogId);
+
+            entity.ToTable("Tbl_Blog");
+
+            entity.Property(e => e.BlogAuthor)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.BlogContent)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.BlogTitle)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
