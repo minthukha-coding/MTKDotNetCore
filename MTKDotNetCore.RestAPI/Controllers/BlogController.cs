70b0fcef29473f0a6275b869929de61e59aa2110
minthukha <minthukha1239788@gmail.com>
2024-04-25 13:46:43 +0630

    [ InProgress ] - BlogAPI With RESTful

--- MTKDotNetCore.RestAPI/Controllers/BlogController.cs
+++ MTKDotNetCore.RestAPI/Controllers/BlogController.cs
@@ -1,18 +1,87 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Mvc;
-using MTKDotNetCore.ConsoleApp.EfCoreExamples;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MTKDotNetCore.ConsoleApp.Modles;
+using MTKDotNetCore.RestAPI.Database;
 
 namespace MTKDotNetCore.RestAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BlogController :ControllerBase
+    public class BlogController : ControllerBase
     {
         private readonly AppDbContext _appDbContext;
 
         public BlogController(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            var lst = _appDbContext.Blogs.ToList();
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlogByBlogID(int id)
+        {
+            var item = _appDbContext.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item == null)
+            {
+                return NotFound("Blog Was Not Found");
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel blog)
+        {
+            _appDbContext.Blogs.Add(blog);
+            var result = _appDbContext.SaveChanges();
+            string message = result > 0 ? "Create Blog Successful." : "Create Blog Failed.";
+            return Ok(message);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id,BlogModel blog)
+        {
+            var item = _appDbContext.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item == null)
+            {
+                return NotFound("Blog Was Not Found");
+            }
+            if (!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                item.BlogTitle = blog.BlogTitle;
+            }
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                item.BlogAuthor = blog.BlogAuthor;
+            }
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                item.BlogContent = blog.BlogContent;
+            }
+            var result = _appDbContext.SaveChanges();
+
+            string message = result > 0 ? "Updating Successful." : "Updating Failed.";
+            return Ok(message);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            var item = _appDbContext.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item == null)
+            {
+                return NotFound("Blog Was Not Found");
+            }
+            _appDbContext.Blogs.Remove(item);
+            var result = _appDbContext.SaveChanges();
+
+            string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
+            return Ok(message);
         }
     }
 }
--- MTKDotNetCore.RestAPI/Services/ConnectionString.cs
+++ MTKDotNetCore.RestAPI/Services/ConnectionString.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
