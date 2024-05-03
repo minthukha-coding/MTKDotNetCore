89fe8e2509c4b9ce1597e9bc679d6f4e82934cf0
minthukha <minthukha1239788@gmail.com>
2024-05-03 16:28:18 +0630

    [ Added ] - FindByID Method

--- MTKDotNetCore.RestAPI/Controllers/BlogDapperController.cs
+++ MTKDotNetCore.RestAPI/Controllers/BlogDapperController.cs
@@ -18,5 +18,24 @@ namespace MTKDotNetCore.RestAPI.Controllers
             List<BlogModel> lst = db.Query<BlogModel>(query).ToList();
             return Ok(lst);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult Getblog(int id)
+        {
+            var item = FindByID(id);
+            if (item == null)
+            {
+                return BadRequest("NO Blog");
+            }
+            return Ok(item);
+        }
+
+        private BlogModel? FindByID(int id)
+        {
+            string query = "select * from tbl_blog where blogid = @BlogId";
+            using IDbConnection db = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+            var item = db.Query<BlogModel>(query,new BlogModel { BlogId = id }).FirstOrDefault();
+            return item;
+        }
     }
 }
