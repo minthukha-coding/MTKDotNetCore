6f87e162075fc0274b015641ecf0b569689e1bf5
minthukha <minthukha1239788@gmail.com>
2024-05-03 16:55:33 +0630

    [ Added ] - AdoDotNetController OF RestAPI Blog

---
+++ MTKDotNetCore.RestAPI/Controllers/BlogAdoDotNetController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MTKDotNetCore.RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogAdoDotNetController : ControllerBase
+    {
+
+    }
+}
