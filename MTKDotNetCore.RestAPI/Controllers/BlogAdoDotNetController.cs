8413c1a75b832758b3fd72f4b0e485c68a64dda7
minthukha <minthukha1239788@gmail.com>
2024-05-05 20:25:15 +0630

    [ OnProgress ] - GetByID With ADODotNet

--- MTKDotNetCore.RestAPI/Controllers/BlogAdoDotNetController.cs
+++ MTKDotNetCore.RestAPI/Controllers/BlogAdoDotNetController.cs
@@ -14,7 +14,7 @@ namespace MTKDotNetCore.RestAPI.Controllers
         {
             string query = "select * from Tbl_Blog";
             SqlConnection connection = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
-            connection.Open();  
+            connection.Open();
             SqlCommand command = new SqlCommand(query, connection);
             SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
             DataTable dt = new DataTable();
@@ -30,6 +30,33 @@ namespace MTKDotNetCore.RestAPI.Controllers
             }).ToList();
 
             return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id)
+        {
+            string query = "select * from Tbl_Blog where Blog_Id = @Blog_Id";
+            SqlConnection connection = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@BlogId", id);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+            if (dt.Rows.Count == 0)
+            {
+                return NotFound("No data found.");
+            }
+            DataRow dr = dt.Rows[0];
+            var item = new BlogModel
+            {
+                BlogId = Convert.ToInt32(dr["BlogId"]),
+                BlogTitle = Convert.ToString(dr["BlogTitle"]),
+                BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+                BlogContent = Convert.ToString(dr["BlogContent"])
+            };
+            return Ok();
         }
     }
 }
