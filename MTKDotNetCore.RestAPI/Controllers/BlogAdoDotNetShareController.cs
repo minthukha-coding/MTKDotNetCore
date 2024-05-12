84c220ad45932b1ca2ba6134ee8a86fbf3fc2cf0
minthukha <minthukha1239788@gmail.com>
2024-05-12 22:45:42 +0630

    [ Complete ] - ADO Custom Service

--- MTKDotNetCore.RestAPI/Controllers/BlogAdoDotNetShareController.cs
+++ MTKDotNetCore.RestAPI/Controllers/BlogAdoDotNetShareController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Reflection.Metadata;
 using ClassLibrary1MTKDotNetCore.Shared;
 using static ClassLibrary1MTKDotNetCore.Shared.ADOService;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace MTKDotNetCore.RestAPI.Controllers
 {
@@ -63,29 +64,17 @@ namespace MTKDotNetCore.RestAPI.Controllers
         [HttpPut("{id}")]
         public IActionResult PutBlogUpdate(int id, BlogModel model)
         {
-            string getQuery = "SELECT COUNT(*) FROM Tbl_Blog  WHERE BlogId = @BlogId";
-            SqlConnection connection = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
-            connection.Open();
-            SqlCommand checkBlogCMD = new SqlCommand(getQuery, connection);
-            checkBlogCMD.Parameters.AddWithValue("@BlogId", id);
-            var count = (int)checkBlogCMD.ExecuteScalar();
-            if (count == 0) return NotFound("No Blog");
-
             string updateQuery = @"UPDATE [dbo].[Tbl_Blog]
                                    SET [BlogTitle] = @BlogTitle,
                                        [BlogAuthor] = @BlogAuthor,
                                        [BlogContent] = @BlogContent
                                    WHERE BlogId = @BlogId";
-
-            SqlCommand cmd = new SqlCommand(updateQuery, connection);
-
-            cmd.Parameters.AddWithValue("@BlogId", id);
-            cmd.Parameters.AddWithValue("@BlogTitle", model.BlogTitle);
-            cmd.Parameters.AddWithValue("@BlogAuthor", model.BlogAuthor);
-            cmd.Parameters.AddWithValue("@BlogContent", model.BlogContent);
-            int result = cmd.ExecuteNonQuery();
-
-            connection.Close();
+            model.BlogId = id;
+            int result = _adoService.Execute(updateQuery,
+                        new ADOParameter("@BlogId", model.BlogId),
+                        new ADOParameter("@BlogTitle", model.BlogTitle),
+                        new ADOParameter("@BlogAuthor", model.BlogAuthor),
+                        new ADOParameter("@BlogContent", model.BlogContent));
             string message = result > 0 ? "Update Blog success" : "Update Blog Fail";
             return Ok(message);
         }
@@ -94,22 +83,7 @@ namespace MTKDotNetCore.RestAPI.Controllers
         public IActionResult DeleteBlog(int id)
         {
             string getQuery = "SELECT COUNT(*) FROM Tbl_Blog  WHERE BlogId = @BlogId";
-            SqlConnection connection = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
-            connection.Open();
-            SqlCommand checkBlogCMD = new SqlCommand(getQuery, connection);
-            checkBlogCMD.Parameters.AddWithValue("@BlogId", id);
-            var count = (int)checkBlogCMD.ExecuteScalar();
-            if (count == 0) return NotFound("No Blog");
-
-            string query = @"DELETE FROM [dbo].[Tbl_Blog]
-                               WHERE BlogId = @BlogId";
-
-            SqlCommand command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@BlogId", id);
-
-            int result = command.ExecuteNonQuery();
-            connection.Close();
-
+            int result = _adoService.Execute(getQuery, new ADOParameter("@BlogId",id));
             string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
             return Ok(message);
         }
@@ -117,73 +91,33 @@ namespace MTKDotNetCore.RestAPI.Controllers
         [HttpPatch("{id}")]
         public IActionResult PatchBlog(int id, BlogModel model)
         {
-            SqlConnection connection = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
-            connection.Open();
-            string query = "select * from Tbl_Blog where BlogId = @BlogId";
-            SqlCommand command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@BlogId", id);
-            var count = (int)command.ExecuteScalar();
-            if (count == 0) return NotFound();
-            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
-            DataTable dt = new DataTable();
-            dataAdapter.Fill(dt);
-            List<BlogModel> lst = new List<BlogModel>();
-            if (dt.Rows.Count == 0)
-            {
-                var respnose = new { IsSuccess = false, Message = "No data not found" };
-                return NotFound(respnose);
-            }
-            DataRow row = dt.Rows[0];
-            BlogModel item = new BlogModel
-            {
-                BlogId = Convert.ToInt32(row["BlogId"]),
-                BlogTitle = Convert.ToString(row["BlogTitle"]),
-                BlogAuthor = Convert.ToString(row["BlogAuthor"]),
-                BlogContent = Convert.ToString(row["BlogContent"]),
-            };
-            lst.Add(item);
-            string conditions = "";
-            List<SqlParameter> parameters = new List<SqlParameter>();
-
-            #region Patch Validation Conditions
-
+            string conditions = string.Empty;
             if (!string.IsNullOrEmpty(model.BlogTitle))
             {
-                conditions += " [BlogTitle] = @BlogTitle, ";
-                parameters.Add(new SqlParameter("@BlogTitle", SqlDbType.NVarChar) { Value = model.BlogTitle });
+                conditions += "[BlogTitle]=@BlogTitle, ";
             }
-
-
             if (!string.IsNullOrEmpty(model.BlogAuthor))
             {
-                conditions += " [BlogAuthor] = @BlogAuthor, ";
-                parameters.Add(new SqlParameter("@BlogAuthor", SqlDbType.NVarChar) { Value = model.BlogAuthor });
-                item.BlogAuthor = model.BlogAuthor;
+                conditions += "[BlogAuthor]=@BlogAuthor, ";
             }
-
             if (!string.IsNullOrEmpty(model.BlogContent))
             {
-                conditions += " [BlogContent] = @BlogContent, ";
-                parameters.Add(new SqlParameter("@BlogContent", SqlDbType.NVarChar) { Value = model.BlogContent });
-                item.BlogContent = model.BlogContent;
+                conditions += "[BlogContent]=@BlogContent, ";
             }
-
+            conditions = conditions.Substring(0, conditions.Length - 2);
             if (conditions.Length == 0)
             {
-                var response = new { IsSuccess = false, Message = "No data found." };
-                return NotFound(response);
+                return NotFound("No Data Found");
             }
-            #endregion
-
-            conditions = conditions.TrimEnd(',', ' ');
-            query = $@"UPDATE [dbo].[Tbl_Blog] SET {conditions} WHERE BlogId = @BlogId";
-            SqlCommand cmd2 = new SqlCommand(query, connection);
-            cmd2.Parameters.AddWithValue("@BlogId", id);
-            cmd2.Parameters.AddRange(parameters.ToArray());
-
-            int result = cmd2.ExecuteNonQuery();
-            connection.Close();
-            string message = result > 0 ? "Patch Updating Successful." : "Patch Updating Failed.";
+            model.BlogId = id;
+            string query = $@"UPDATE [dbo].[Tbl_Blog]
+                            SET {conditions} 
+                            WHERE BlogId=@BlogId";
+            int result = _adoService.Execute(query, new ADOParameter("@BlogId", model.BlogId),
+                                                         new ADOParameter("@BlogTitle", model.BlogTitle),
+                                                         new ADOParameter("@BlogAuthor", model.BlogAuthor),
+                                                         new ADOParameter("@BlogContent", model.BlogContent));
+            string message = result > 0 ? "Blog update successful" : "Blog update fail";
             return Ok(message);
         }
     }
