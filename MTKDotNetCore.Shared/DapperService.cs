c94bd1a3c7b22474156ab70564404f615719a229
minthukha <minthukha1239788@gmail.com>
2024-05-12 20:15:55 +0630

    [ Dapper Service ] - Added

---
+++ MTKDotNetCore.Shared/DapperService.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace ClassLibrary1MTKDotNetCore.Shared;
+
+public class DapperService
+{
+    private readonly string _connectionString;
+
+    public DapperService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    
+    public List<G> Query<G>(string query,object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_connectionString);
+        var lst = db.Query<G>(query,param).ToList();
+        return lst;
+    }  
+    public G QueryFirstOrDefault<G>(string query,object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_connectionString);
+        var item = db.Query<G>(query,param).FirstOrDefault();
+        return item!;
+    }
+
+    public int Excute(string query, object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_connectionString);
+        var result =  db.Execute(query, param);
+        return result;
+    }
+}
