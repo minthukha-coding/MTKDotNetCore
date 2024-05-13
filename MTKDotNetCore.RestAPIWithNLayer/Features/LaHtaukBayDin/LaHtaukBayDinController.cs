1e3048f0b16b8b1faa79a0ddf1efc0d24b3936b4
minthukha <minthukha1239788@gmail.com>
2024-05-14 00:10:59 +0630

    [ Complete ] - Added OF NLayer in LaHTaukBayDin API

--- MTKDotNetCore.RestAPIWithNLayer/Features/LaHtaukBayDin/LaHtaukBayDinController.cs
+++ MTKDotNetCore.RestAPIWithNLayer/Features/LaHtaukBayDin/LaHtaukBayDinController.cs
@@ -8,6 +8,7 @@ namespace MTKDotNetCore.RestAPIWithNLayer.Features.LaHtaukBayDin
     [ApiController]
     public class LaHtaukBayDinController : ControllerBase
     {
+
         private LaHtaukBayDin _data;
 
         private async Task<LaHtaukBayDin> GetDataAsync()
@@ -24,12 +25,26 @@ namespace MTKDotNetCore.RestAPIWithNLayer.Features.LaHtaukBayDin
             return Ok(model.questions);
         }
 
+        [HttpGet("numberList")]
+        public async Task<IActionResult> NumberList()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.numberList);
+        }
+
+        [HttpGet("{questionNo}/{no}")]
+        public async Task<IActionResult> Answer(int questionNo, int no)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.answers.FirstOrDefault(x => x.questionNo == questionNo && x.answerNo == no));
+        }
+
         #region Model
 
         public class LaHtaukBayDin
         {
             public Question[] questions { get; set; }
-            public Answer[] answers { get; set; }
+            public AnswerCS[] answers { get; set; }
             public string[] numberList { get; set; }
         }
 
@@ -39,14 +54,14 @@ namespace MTKDotNetCore.RestAPIWithNLayer.Features.LaHtaukBayDin
             public string questionName { get; set; }
         }
 
-        public class Answer
+        public class AnswerCS
         {
             public int questionNo { get; set; }
             public int answerNo { get; set; }
             public string answerResult { get; set; }
         }
-
-        #endregion
-
     }
+
+    #endregion
+
 }
