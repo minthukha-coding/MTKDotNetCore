04648329835ae6421b78d2c584b3f9326be098ce
minthukha <minthukha1239788@gmail.com>
2024-05-13 23:31:58 +0630

    [ InProgress ] - ConsoleAPP With HTTPClient

--- MTKDotNetCore.ConsoleAppHTTPClient/Program.cs
+++ MTKDotNetCore.ConsoleAppHTTPClient/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
 Console.WriteLine("Hello, World!");
+
+string jsonstr = await File.ReadAllTextAsync("data.json");
+var model = JsonConvert.DeserializeObject<MinTheinKha>(jsonstr);
+
+Console.WriteLine(model);
+
+Console.ReadKey();
+public class MinTheinKha
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
