c60f3cc1bea6ff0a960029f42f0c10434fad00e1
minthukha <minthukha1239788@gmail.com>
2024-05-16 02:39:34 +0630

    [ InProgrss ] - HTTPCleint

--- MTKDotNetCore.HTTPClient/HTTPClientExample.cs
+++ MTKDotNetCore.HTTPClient/HTTPClientExample.cs
@@ -4,14 +4,19 @@ namespace MTKDotNetCore.ConsoleApp.HTTPClient
 {
     public class HTTPClientExample
     {
+        private readonly HttpClient _httpClient = new HttpClient() { BaseAddress = new Uri("https://localhost:7051") };
+        private readonly string _blogEndPoint = "api/blogAdoDotNet";
+
         public void Run()
         {
+            Edit(7);
+            Edit(422);
             Read();
         }
+
         private async Task Read()
         {
-            HttpClient httpClient = new HttpClient();
-            HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7051/api/blogAdoDotNet");
+            var response = await _httpClient.GetAsync(_blogEndPoint);
             if (response.IsSuccessStatusCode)
             {
                 string jsonStr = await response.Content.ReadAsStringAsync();
@@ -24,7 +29,25 @@ namespace MTKDotNetCore.ConsoleApp.HTTPClient
                     Console.WriteLine($"Content - {blog.BlogContent}");
                     Console.WriteLine("_____________");
                 }
-                Console.ReadLine();
+            }
+        }
+
+        private async Task Edit(int id)
+        {
+            var response = await _httpClient.GetAsync($"{_blogEndPoint}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                var item = JsonConvert.DeserializeObject<BlogModel>(jsonStr)!;
+                Console.WriteLine($"Title - {item.BlogTitle}");
+                Console.WriteLine($"Author - {item.BlogAuthor}");
+                Console.WriteLine($"Content - {item.BlogContent}");
+                Console.WriteLine("_____________");
+            }
+            else
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
             }
         }
     }
--- MTKDotNetCore.HTTPClient/Program.cs
+++ MTKDotNetCore.HTTPClient/Program.cs
@@ -1,23 +1,4 @@
-﻿//Console.WriteLine("Hello, World!");
-
-//HttpClient httpClient = new HttpClient();
-//HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7051/api/blogAdoDotNet");
-//if (response.IsSuccessStatusCode)
-//{
-//    string jsonStr = await response.Content.ReadAsStringAsync();
-//    List<BlogModel> lst = JsonConvert.DeserializeObject<List<BlogModel>>(jsonStr)!;
-//    foreach (var blog in lst)
-//    {
-//        //Console.WriteLine(JsonConvert.SerializeObject(blog));
-//        Console.WriteLine($"Title - {blog.BlogTitle}");
-//        Console.WriteLine($"Author - {blog.BlogAuthor}");
-//        Console.WriteLine($"Content - {blog.BlogContent}");
-//        Console.WriteLine("_____________");
-//    }
-//    Console.ReadLine();
-//}
-
-using MTKDotNetCore.ConsoleApp.HTTPClient;
+﻿using MTKDotNetCore.ConsoleApp.HTTPClient;
 
 HTTPClientExample ex = new HTTPClientExample();
 ex.Run();
