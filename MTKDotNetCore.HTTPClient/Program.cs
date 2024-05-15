2d02303d1ad0cb9160f0ea59e4c020255eadd324
minthukha <minthukha1239788@gmail.com>
2024-05-15 13:18:10 +0630

    [ HTTPClient ] - Request API & Show Data

--- MTKDotNetCore.HTTPClient/Program.cs
+++ MTKDotNetCore.HTTPClient/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+HttpClient httpClient = new HttpClient();
+HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7279/api/movieticket/getmovies");
+if (response.IsSuccessStatusCode)
+{
+    string jsonStr = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(jsonStr);
+    Console.ReadKey();  
+}   
