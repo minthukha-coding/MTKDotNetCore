4cd705c3e03b96fef45cdd7ff4a1708479f10221
minthukha <minthukha1239788@gmail.com>
2024-05-19 19:52:53 +0630

    [ InProgress ] - MyanmarProverbs

--- MTKDotNetCore.RestAPIWithNLayer/Features/MyanmarProverbs/MyanmarProverbsController.cs
+++ MTKDotNetCore.RestAPIWithNLayer/Features/MyanmarProverbs/MyanmarProverbsController.cs
@@ -1,11 +1,45 @@
-﻿ using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
-namespace MTKDotNetCore.RestAPIWithNLayer.Features.MyanmarProverbs
+namespace MTKDotNetCore.RestAPIWithNLayer.Features.MyanmarProverbs;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MyanmarProverbsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MyanmarProverbsController : ControllerBase
+    private async Task<Myanmarproverbs> GetDataFromApi()
     {
+        HttpClient httpClient = new HttpClient();
+        var response = await httpClient.GetAsync("https://github.com/sannlynnhtun-coding/Myanmar-Proverbs/blob/main/MyanmarProverbs.json");
+        if (response.IsSuccessStatusCode)
+        {
+            string jsonStr = await response.Content.ReadAsStringAsync();
+            JsonConvert.DeserializeObject<Myanmarproverbs>(jsonStr);
+        }
     }
+    public async Task<IActionResult> Get()
+    {
+        var model = await GetDataFromApi();
+        return Ok(model.Tbl_MMProverbsTitle);
+    }
+}
+public class Myanmarproverbs
+{
+    public Tbl_Mmproverbstitle[] Tbl_MMProverbsTitle { get; set; }
+    public Tbl_Mmproverbs[] Tbl_MMProverbs { get; set; }
+}
+
+public class Tbl_Mmproverbstitle
+{
+    public int TitleId { get; set; }
+    public string TitleName { get; set; }
+}
+
+public class Tbl_Mmproverbs
+{
+    public int TitleId { get; set; }
+    public int ProverbId { get; set; }
+    public string ProverbName { get; set; }
+    public string ProverbDesp { get; set; }
 }
