0a59f163a9370f023506d0838456502aef6db98e
minthukha <minthukha1239788@gmail.com>
2024-05-19 19:42:48 +0630

    [ Added ] - Myanmar Proverbs In RestAPIWithNLayer Project

---
+++ MTKDotNetCore.RestAPIWithNLayer/Features/MyanmarProverbs/MyanmarProverbsController.cs
@@ -0,0 +1,11 @@
+﻿ using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MTKDotNetCore.RestAPIWithNLayer.Features.MyanmarProverbs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyanmarProverbsController : ControllerBase
+    {
+    }
+}
