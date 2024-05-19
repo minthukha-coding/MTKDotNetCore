0f2c32348b451cd7e5de717aa59fb5ebb517e1a8
minthukha <minthukha1239788@gmail.com>
2024-05-19 12:33:53 +0630

    [Added ] - IBlogApi

---
+++ MTKDotNetCore.ConsoleApp.RefitExample/IBlogApi.cs
@@ -0,0 +1,27 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCore.ConsoleApp.RefitExample
+{
+    public interface IBlogApi
+    {
+        [Get("api/blogEfCore")]
+        Task<List<BlogModel>> GetBlogs();
+
+        [Get("api/blogEfCore/{id}")]
+        Task<BlogModel> GetBlog(int blogId);
+        [Post("api/blogEfcore")]
+        Task<BlogModel> CreateBlog(BlogModel blog);
+    }
+    public class BlogModel
+    {
+        public int BlogId { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
+    }
+}
