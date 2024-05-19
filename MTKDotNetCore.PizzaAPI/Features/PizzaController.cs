b12a5870301ab08b6f4b01adf14d0c47e7b978ea
minthukha <minthukha1239788@gmail.com>
2024-05-19 23:21:34 +0630

    [ Config ] - Configure Setup Of PizzaAPI

---
+++ MTKDotNetCore.PizzaAPI/Features/PizzaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MTKDotNetCore.PizzaAPI.Features
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+    }
+}
