e7c7bd6ce6f0e6a35bf370a03b9da090a6216dc8
minthukha <minthukha1239788@gmail.com>
2024-05-26 00:59:40 +0630

    [ Added ] - PizzaQuery

---
+++ MTKDotNetCore.PizzaAPI/Queries/PizzaQuery.cs
@@ -0,0 +1,15 @@
+﻿namespace MTKDotNetCore.PizzaAPI.Queries
+{
+    public class PizzaQuery
+    {
+        public static string PizzaOrderQuery { get; } =
+            @"select po.* , p.Pizza, p.Price from [dbo] . [Tbl_PizzaOrder] po
+            inner join Tbl_Pizza  p on p.PizzaId = po.PizzaId
+            where PizzaOrderInoviaceNo = @PizzaOrderInoviaceNo";
+
+        public static string PizzaOrderDetailQuery { get; } =
+            @"select pod.*,px.PizzaExtraName,px.Price from [dbo] . [Tbl_PizzaOrderDetailModel] pod
+             inner join Tbl_PizzaExtra px on px.PizzaExtraId = pod.PizzaExtraId
+             where PizzaOrderInoviaceNo = @PizzaOrderInoviaceNo";
+    }
+}
