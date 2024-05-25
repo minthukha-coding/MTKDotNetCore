4218a53e535514059dc2a4d16507016ae017c470
minthukha <minthukha1239788@gmail.com>
2024-05-26 04:24:15 +0630

    [ Added ] - Model

---
+++ MTKDotNetCore.WindowFormsApp/Models/BlogModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MTKDotNetCore.WindowFormsApp.Models;
+
+public class BlogModel
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
