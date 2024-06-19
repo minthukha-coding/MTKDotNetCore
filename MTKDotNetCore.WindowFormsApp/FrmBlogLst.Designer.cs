a855e2e3781bf439089ff475feb99486a1070623
minthukha <minthukha1239788@gmail.com>
2024-06-19 22:58:47 +0630

    [ InProgress ] - WindowFormLst

--- MTKDotNetCore.WindowFormsApp/FrmBlogLst.Designer.cs
+++ MTKDotNetCore.WindowFormsApp/FrmBlogLst.Designer.cs
@@ -29,18 +29,49 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            colID = new DataGridViewTextBoxColumn();
+            colTitle = new DataGridViewTextBoxColumn();
+            colAuthor = new DataGridViewTextBoxColumn();
+            colContent = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { colID, colTitle, colAuthor, colContent });
             dataGridView1.Dock = DockStyle.Fill;
             dataGridView1.Location = new Point(0, 0);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
             dataGridView1.Size = new Size(800, 450);
             dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // colID
+            // 
+            colID.HeaderText = "ID";
+            colID.MinimumWidth = 8;
+            colID.Name = "colID";
+            // 
+            // colTitle
+            // 
+            colTitle.HeaderText = "Title";
+            colTitle.MinimumWidth = 8;
+            colTitle.Name = "colTitle";
+            // 
+            // colAuthor
+            // 
+            colAuthor.HeaderText = "Author";
+            colAuthor.MinimumWidth = 8;
+            colAuthor.Name = "colAuthor";
+            // 
+            // colContent
+            // 
+            colContent.HeaderText = "Content";
+            colContent.MinimumWidth = 8;
+            colContent.Name = "colContent";
             // 
             // FrmBlogLst
             // 
@@ -57,5 +88,9 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn colID;
+        private DataGridViewTextBoxColumn colTitle;
+        private DataGridViewTextBoxColumn colAuthor;
+        private DataGridViewTextBoxColumn colContent;
     }
 }
--- MTKDotNetCore.WindowFormsApp/FrmBlogLst.cs
+++ MTKDotNetCore.WindowFormsApp/FrmBlogLst.cs
@@ -16,5 +16,10 @@ namespace MTKDotNetCore.WindowFormsApp
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
